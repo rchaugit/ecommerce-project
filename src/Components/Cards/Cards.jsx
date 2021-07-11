5c8a6de931fd982bdf951bfd93fe53e6080d3bcc
rchaugit <84691526+rchaugit@users.noreply.github.com>
2021-07-11 23:14:35 +1000

    Revert "First update"
    This reverts commit 528c72081a9bf3f7f018586ade044681ac543c9a.

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,6 +1,6 @@
 import styles from './Cards.module.scss'
 
-const Cards = ( { product, addToCart }) => {
+const Cards = ( { product, toCart }) => {
 
   return (
     <>
@@ -8,11 +8,9 @@ const Cards = ( { product, addToCart }) => {
         {product.map((product) => (
           <div className={styles.catalog__product} key={product.id}>
             <img className={styles.catalog__img} src={product.img}></img>
-            <p className={styles.catalog__item}>{product.item} </p>
-            <p className={styles.catalog__price}>Price: ${product.price}</p>
-            <button onClick = {() => 
-              {addToCart({product})
-              }}>Add to Cart</button>
+            <p>Item: {product.item} </p>
+            <p>Price: {product.Price}</p>
+            <button onClick={toCart}>Add to Cart</button>
           </div>
         ))}
       </div>
